3e747a1b267e9084489f0e1f46433d554c020546
nrcool <nomanalam7214@gmail.com>
2022-05-09 14:21:48 +0200

    solution

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 // 1
 function uppercase(str) {
   //write your code here 
+  const splitedText = str.split(" ")
+  let result =""
+  for(let i=0; i<splitedText.length;i++){
+    result+= splitedText[i].charAt(0).toUpperCase()+splitedText[i].slice(1) +" "
+   /*  result.push(splitedText[i].charAt(0).toUpperCase()+splitedText[i].slice(1) ) */
+  /*   result.push(splitedText[i][0].toUpperCase()+splitedText[i].slice(1) ) */
+  }
+  return result
 }
 
 console.log(uppercase("the quick brown fox"));
@@ -10,6 +18,16 @@ console.log(uppercase("the quick brown fox"));
 // Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
 function find_longest_word(str) {
   //write your code here 
+  const splitedText= str.split(" ")
+
+  let longestWord = splitedText[0];
+  for(let i = 0; i < splitedText.length ; i++){
+    if(longestWord.length < splitedText[i].length){
+      longestWord = splitedText[i]
+    }
+  }
+  return longestWord;
+
 }
 console.log(find_longest_word("Web Development Tutorial"));
 
@@ -17,6 +35,14 @@ console.log(find_longest_word("Web Development Tutorial"));
 // Write a JavaScript function that accept a list of country names as input and returns the longest country name as output.
 function Longest_Country_Name(country_name) {
   //write your code here 
+
+  let longestWord = country_name[0];
+  for(let i = 0; i < country_name.length ; i++){
+    if(longestWord.length < country_name[i].length){
+      longestWord = country_name[i]
+    }
+  }
+  return longestWord;
 }
 
 console.log(
